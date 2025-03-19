393abb31c3feb3f9c0c02c6eb45f33804e21263f
ny-haritina10 <142341722+ny-haritina10@users.noreply.github.com>
2025-03-19 20:06:33 +0300

    [FIX] input text in main

--- src/mg/itu/main/Main.java
+++ src/mg/itu/main/Main.java
@@ -1,6 +1,5 @@
 package mg.itu.main;
 
-import java.io.FileWriter;
 import java.io.IOException;
 
 import mg.itu.compressor.FileCompressor;
@@ -29,18 +28,15 @@ public class Main {
         
     //     // SwingUtilities.invokeLater(coder::visualizeTree);
     // }
+    
 
+    // Partie - 2
     public static void main(String[] args) {
         try {
             FileCompressor compressor = new FileCompressor();
 
             String inputFile = "D:\\Studies\\ITU\\S6\\INF-310_Codage\\text-encoder\\__input_text.txt";
             String outputFile = "D:\\Studies\\ITU\\S6\\INF-310_Codage\\text-encoder\\__output_text.huff";
-            
-            // Create a sample text file if it doesn't exist
-            try (FileWriter writer = new FileWriter(inputFile)) {
-                writer.write("huffman coding example");
-            }
             
             compressor.compressFile(inputFile, outputFile);
             System.out.println("File compressed successfully to " + outputFile);
